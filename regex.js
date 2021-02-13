77985f407bf56a4adafbd8f42069102ae0c5c2e8
Dorin <d.dumitrascuta@gmail.com>
2021-02-13 23:25:52 +0000

    0 or more chars

--- regex.js
+++ regex.js
@@ -1,1 +1,6 @@
 /[^ABCD]/ // this is a negated character set (selects everything that is not in set 
+/HE|LL|O+/ // this | pipe operator acts as an OR selector HE or LL, zero or more tokens
+/*/ // this actually matches 0 or more
+
+
+
